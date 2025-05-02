490025f9be252cb6d03c63249b70b9151106e5ff
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-02 08:21:47 +0700

    menambahkan void binary

---
+++ first1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int element[10];
+int nPanjang;
+int x;
+
+void binarySearch() {
+    cout << "Masukkan element yang ingindicari =";
+    cin >> x;  // step 1
+    int low = 0;  // step 2
+    int high = nPanjang-1; // step 3
+    int mid = (lowhigh)/2; //step 4
+    if (element[mid] == x) { // step 5
+        cout << "Ditemukan" << endl; // step 5a
+    }
+}
+
+int main(){
+
+}
