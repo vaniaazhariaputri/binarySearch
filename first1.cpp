e61331370265e7789cb5c49657f7fb5bdae3c996
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-02 09:23:40 +0700

    menambahkan void input

--- first1.cpp
+++ first1.cpp
@@ -5,6 +5,33 @@ int element[10];
 int nPanjang;
 int x;
 
+void input()
+{ // Prosedur input
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array:";
+        cin >> nPanjang;
+        if (nPanjang <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray maksimal 20 elemen.\n";
+        }
+    }
+
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << "=";
+        cin >> element[i];
+    }
+}
 
 
 void binarySearch()
